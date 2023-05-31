971de90d619b01d4d363cf0b06c83b4d9b784976
michaellhz <nacheal@qq.com>
2023-05-31 15:12:16 +0800

    add

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
-// const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-const response = await openai.createCompletion({
+const response = openai.createCompletion({
   model: "text-davinci-003",
   prompt:
     'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ:',
@@ -19,4 +19,6 @@ const response = await openai.createCompletion({
   stop: ["\n"],
 });
 
-console.log("michae", response.data.choices[0].text);
+response.then((response) => {
+  console.log("michae", response.data.choices[0].text);
+});
