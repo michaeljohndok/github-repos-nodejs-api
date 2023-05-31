795bab5a1e0080c76fd1f3913297493801944e12
Guoquan mai <dazakeen@gmail.com>
2023-05-31 15:07:14 +0800

    Update index.js 3

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const PORT = process.env.PORT || 8081;
+import { Configuration, OpenAIApi } from "openai";
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/michaeljohndok/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+// const { Configuration, OpenAIApi } = require("openai");
 
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt:
+    'I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ:',
+  temperature: 0,
+  max_tokens: 100,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+  stop: ["\n"],
 });
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+console.log("michae", response.data.choices[0].text);
